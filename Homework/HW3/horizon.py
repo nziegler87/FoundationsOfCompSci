d321fa1ea2fdd04567a8187ee8a8380bdec1e4be
Nathanial Ziegler <ziegler.n@husky.neu.edu>
2020-01-27 18:06:11 -0500

    Assigned Constants
    Assigned constants

--- Homework/HW3/horizon.py
+++ Homework/HW3/horizon.py
@@ -18,13 +18,19 @@ import turtle
 SUN_RADIUS = 100
 MOON_RADIUS = 100
 SCREEN_SIZE = 800
+Y_CORD = 200
+MOON_X_CORD = -475
+SUN_X_CORD = 150
+DAYLIGHT_SKY = "#C9EEFF"
+ECLIPSE_SKY = "#738993"
+ANIMATION_SPEED = 3
 
 def main():
 
     # Set screen and initial canvas color
     screen = turtle.Screen()
     screen.setup(SCREEN_SIZE, SCREEN_SIZE)
-    screen.bgcolor("#C9EEFF")
+    screen.bgcolor(DAYLIGHT_SKY)
     screen.tracer(0)
 
     # Add sun and moon shapes
@@ -37,28 +43,29 @@ def main():
     sun = turtle.Turtle()
     sun.speed(0)
     sun.up()
-    sun.setpos(150, 200)
+    sun.setpos(SUN_X_CORD, Y_CORD)
     sun.shape(sun_gif)
 
     # Create moon turtle
     moon = turtle.Turtle()
     moon.speed(0)
     moon.up()
-    moon.setpos(-475, 200)
+    moon.setpos(MOON_X_CORD, Y_CORD)
     moon.shape(moon_gif)
 
     # Animation loop
     while True:
+        moon.forward(ANIMATION_SPEED)
         screen.update()
+
         # Stop animation once moon is off the screen
-        if moon.xcor() == ((SCREEN_SIZE / 2) + MOON_RADIUS):
-            break
-        # Increment sun forward. Change background color when moon and sun overlap
-        if moon.xcor() <= (sun.xcor() - SUN_RADIUS) or moon.xcor() >= (sun.xcor() + SUN_RADIUS):
-            screen.bgcolor("#C9EEFF")
-            moon.forward(5)
+        if moon.xcor() > ((SCREEN_SIZE / 2) + MOON_RADIUS):
+           screen.bye()
+
+        # Increment sun forward; change background color when moon and sun overlap
+        elif moon.xcor() <= (sun.xcor() - SUN_RADIUS) or moon.xcor() >= (sun.xcor() + SUN_RADIUS):
+            screen.bgcolor(DAYLIGHT_SKY)
         else:
-            screen.bgcolor("#738993")
-            moon.forward(5)
+            screen.bgcolor(ECLIPSE_SKY)
             
 main()
