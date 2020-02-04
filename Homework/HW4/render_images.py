8f5d0016c9ab4ac7b36569228bf9a3dcf064b5e4
Nathanial Ziegler <ziegler.n@husky.neu.edu>
2020-02-04 14:37:02 -0500

    Update render_images.py

--- Homework/HW4/render_images.py
+++ Homework/HW4/render_images.py
@@ -16,7 +16,7 @@ LITTLE_IMG = [[L, L, L, L, R, R, R, R, L, L, L, L],
               [L, L, R, R, R, L, R, R, L, R, R, L]]
 
 BIG_IMG = [[P, P, P, P, P, P, P, Y, P, P, Y, Y, Y, Y, P, Y, Y, Y, P, Y, P, Y, P, P, P, P, P, P, P, Y, Y, Y], 
-           [P, Y, Y, Y, Y, Y, P, Y, Y, Y, Y, Y, Y, P, Y, Y, P, Y, P, Y, P, Y, P, Y, Y, Y, Y, Y, P, Y, Y,  Y], 
+           [P, Y, Y, Y, Y, Y, P, Y, Y, Y, Y, Y, Y, P, Y, Y, P, Y, P, Y, P, Y, P, Y, Y, Y, Y, Y, P, Y, Y, Y], 
            [P, Y, P, P, P, Y, P, Y, P, Y, P, P, P, Y, Y, P, Y, Y, P, Y, P, Y, P, Y, P, P, P, Y, P, Y, Y, Y], 
            [P, Y, P, P, P, Y, P, Y, P, P, Y, Y, Y, Y, P, P, P, Y, Y, Y, P, Y, P, Y, P, P, P, Y, P, Y, Y, Y], 
            [P, Y, P, P, P, Y, P, Y, P, P, Y, P, P, Y, Y, P, P, P, P, Y, Y, Y, P, Y, P, P, P, Y, P, Y, Y, Y], 
