543ecc13e33dd305b9f9dae9de9b9cd7c30b83f8
Nathanial Ziegler <ziegler.n@husky.neu.edu>
2020-02-04 21:13:59 -0500

    Finished football HW

--- Homework/HW4/football_functions.py
+++ Homework/HW4/football_functions.py
@@ -37,13 +37,21 @@ def print_all_results(results, result_options):
     ''' Name: print_all_results
         Input: list of strings
         Return: nothing
-        Does: Calls count_result functions, passing each itteration of
+        Does: Calls count_result functions, passing each value in
               result_options to count_result functions and prints output
     '''
     for i in range(len(result_options)):
         outcome = count_result(results, result_options[i])
         print("\t", result_options[i], ": ", outcome, sep = "")
     print()
+
+def print_results_question():
+    ''' Name: print_results_question
+        Input: nothing
+        Returns: nothing
+    '''
+    print("How many games did Tottenham win, lose, and draw over",
+          "the two seasons?")
 
 def count_one_wins(result_list, goals_list):
     ''' Name: count_one_wins
@@ -60,24 +68,41 @@ def count_one_wins(result_list, goals_list):
 def compile_streaks(result_list):
     ''' Name: compile_streaks
         Input: list of results, each item a string
-        Returns: list of results, with streaks combined
+        Returns: string of results with streaks combined
     '''
     list_length = len(result_list)
     count = 1
     streak_list = []
+
+    # Iterate through each item in result_list
     for i in range(len(result_list)):
+
+        # Set potential item or streak to be added to temp string
         streak = (str(count) + result_list[i])
+
+        # Iterate through every item in list, except last one
         if i < (list_length - 1):
+
+            # Account for chance that list has only one item
             if list_length == 1:
                 streak_list.append(streak)
+
+            # Update streak count if current item equals next
             elif result_list[i] == result_list[(i + 1)]:
                 count += 1
+
+            # If current item != next, append current streak or item
             else:
                 streak_list.append(streak)
                 count = 1
+
+        # Append either last item or current streak
         else:
             streak_list.append(streak)
+
+    # Call convert_to_string to join items in list as a string
     final_string = convert_to_string(streak_list)
+
     return final_string
 
 def convert_to_string(original_list):
@@ -85,12 +110,16 @@ def convert_to_string(original_list):
         Input: list of strings
         Returns: list of strings joined as one string
     '''
+    # Set blank string
     output_string = ""
+
+    # Iterate over list of strings, adding each to blank string
     for i in range(len(original_list)):
         if i < (len(original_list) - 1):
             output_string += (original_list[i] + " ")
         else:
             output_string += (original_list[i])
+
     return output_string
 
 def sum_points(result_list, season, game_number):
@@ -99,10 +128,15 @@ def sum_points(result_list, season, game_number):
                season and game_number - ints
         Returns: number of points up to season and game_number as int
     '''
+    # Convert season and game_number to total games played
     game = (season - 1) * GAMES_IN_SEASON
     game += game_number
+
+    # Copy result_list up to games played
     short_list = result_list[:game]
     total = 0
+
+    # Iterate through list, adding point values based on result
     for i in short_list:
         if i == "W":
             total += WIN_POINTS
@@ -110,23 +144,36 @@ def sum_points(result_list, season, game_number):
             total += DRAW_POINTS
         else:
             total += LOSS_POINTS
+
     return total
+
+def print_sum_question():
+    ''' Name: print_sum_question
+        Input: nothing
+        Return: nothing
+    '''
+    print("How many points did Tottenham have after the 20th game",
+          "of the second season in the list?")
 
 def average_score(result_list, goals_list):
     ''' Name: average_score
-        Input: list of results (strs), list of goals (ints),
-               outcome (str)
-        Returns: average score (float)
+        Input: list of results (strs), list of goals (ints)
+        Returns: average score rounded to two decimals (float)
         Does: Calculates the average score for games that were won
     '''
     count = 0
     total = 0
+
+    # Iterate over result list adding number of goals for each win
     for i in range(len(result_list)):
         if result_list[i] == WIN:
             count += 1
             total += goals_list[i]
+
+    # Calculate average of goals for games won
     average = total / count
     average = round(average, 2)
+    
     return average
 
 def print_with_delay(string):
@@ -135,5 +182,6 @@ def print_with_delay(string):
         Returns: Nothing
         Does: Prints and then delays to next line
     '''
+    # Print inputed string and delays printing of next
     print(string, "\n")
     time.sleep(2)      
--- Homework/HW4/football_stats.py
+++ Homework/HW4/football_stats.py
@@ -10,6 +10,7 @@
 from football_functions import *
 import time
 
+# Constants for functions
 POSSIBLE_RESULTS = ["W", "L", "D"]
 
 RESULTS = ['W', 'L', 'D', 'W', 'D', 'W', 'W', 'W', 'W', 'L',
@@ -28,23 +29,27 @@ GOALS = [2, 1, 1, 3, 0, 3, 4, 1, 4, 0, 1, 0, 1, 1, 1,
          2, 2, 1, 3, 1, 0, 1, 1, 1, 2, 4, 0, 1, 0, 0, 2]
 
 def main():
-    print("How many games did Tottenham win, lose, and draw over the two seasons?")
+    # Print number of games, won, lost, and drawn over the seasons
+    print_results_question()
     print_all_results(RESULTS, POSSIBLE_RESULTS)
     time.sleep(2)
 
+    # Print number of games won despite scoring only one goal
     print("How many games did we win, despite scoring only one goal?")
     wins = count_one_wins(RESULTS, GOALS)
     print_with_delay(wins)
 
+    # Print streaks across the season
     print("What did our “streaks” look like over the season?")
     streaks = compile_streaks(RESULTS)
     print_with_delay(streaks)
 
-    print("How many points did Tottenham have after the 20th game of the second ",
-          "season in the list?")
+    # Print total points after Season 2, Game 20
+    print_sum_question()
     points = sum_points(RESULTS, 2, 20)
     print_with_delay(points)
-    
+
+    # Print average score for all games won
     print("What is the average score for all games won across the season?")
     average = average_score(RESULTS, GOALS)
     print_with_delay(average)
