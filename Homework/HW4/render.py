8b664ecdd24cccc2100bb226f775f146898cf029
Nathanial Ziegler <ziegler.n@husky.neu.edu>
2020-02-05 01:47:35 -0500

    Finished render

--- Homework/HW4/render.py
+++ Homework/HW4/render.py
@@ -8,19 +8,19 @@
         string of pixels and then uses Turtle to render image on screen.        
 '''
 
+from render_images import *
+from compress_functions import *
+import turtle
+
+# Image compression and rendering constants
 ROW_PER_PAGE = 3
 COL_PER_PAGE = 4
-PIXEL_PER_LINE = 4
 PIXEL_SIZE = 10
 IMAGE_OPTIONS = ["BIG", "LITTLE"]
 
 # Constants for draw_pixel function
 SIDES_IN_SQUARE = 4
 RIGHT_TURN = 90
-
-from render_images import *
-from compress_functions import *
-import turtle
 
 def validate_input(user_input, option_list):
     ''' Name: validate_input
@@ -83,7 +83,7 @@ def collect_image_size(option_list):
         Input: list of user options, each as string
         Returns: name of file to be generated
     '''
-    # Ask if user wants to render the BIG or LITLE image
+    # Ask if user wants to render the BIG or LITTLE image
     selection = input("Enter BIG or LITTLE: ").upper()
 
     # Call validate input to ensure response is valid
@@ -96,14 +96,26 @@ def collect_image_size(option_list):
 
     return file
 
-def set_start_cord(draw_turtle, original_list, PIXEL_SIZE):
-    '''
-        Does: sets turtle to render pixels at starting cord,
-              based on pixel list and size
+def set_start_cord(draw_turtle, uncompressed_img, pixel_size, row_per_page,
+                   col_per_page):
+    ''' Name: set_start_cord
+        Input: draw_turtle name, uncompressed image (nested list of strings),
+               pixel_size (int)
+        Returns: nothing
+        Does: sets turtle to x, y cord so image is centered on page
     '''
     draw_turtle.up()
-    y_cord = determine_y_start(original_list, PIXEL_SIZE)
-    x_cord = determine_x_start(original_list, PIXEL_SIZE)
+
+    # Calculate y cord using original image
+    num_row = calculate_row_pages(uncompressed_img, row_per_page)
+    y_size = num_row * row_per_page * pixel_size
+    y_cord = y_size / 2
+
+    # Calcuate x cord using original image 
+    num_col = calculate_col_pages(uncompressed_img, col_per_page)
+    x_size = num_col * col_per_page * pixel_size
+    x_cord = x_size / 2
+    
     draw_turtle.goto(-(x_cord),(y_cord))
 
 def draw_pixel(turtle, color, pixel_size):
@@ -112,13 +124,18 @@ def draw_pixel(turtle, color, pixel_size):
         Returns: nothing
         Does: Draws one "pixel" based on pixel size and color
     '''
+    # Set turtle initial state; set to down incase not reaady to write
     turtle.hideturtle()
     turtle.down()
     turtle.color(color, color)
     turtle.begin_fill()
+
+    # Iterate through four loop to make square
     for i in range(SIDES_IN_SQUARE):
         turtle.forward(pixel_size)
         turtle.right(RIGHT_TURN)
+
+    # Fill pixel and prepare turtle for move
     turtle.end_fill()
     turtle.up()
 
@@ -150,12 +167,12 @@ def draw_page(turtle, pixel_list):
     '''
     # Iterate through each row, rendering pixels
     j = 0
-    for i in range(1, PIXEL_PER_LINE):
+    for i in range(1, COL_PER_PAGE):
         start_x = turtle.xcor()
         start_y = turtle.ycor()
         
         # Render pixel for each row using pixel_to_color and draw_pixel functions
-        while j < ((i * PIXEL_PER_LINE)):
+        while j < ((i * COL_PER_PAGE)):
             color = pixel_to_color(pixel_list[j])
             draw_pixel(turtle, color, PIXEL_SIZE)
             turtle.forward(10)
@@ -163,122 +180,55 @@ def draw_page(turtle, pixel_list):
 
         # Move turtle to starting point but down a row of pixles
         turtle.goto(start_x, (start_y - PIXEL_SIZE))
-               
 
-def draw_image(turtle, image_list):
-    image_col = calculate_col_pages(image_list, COL_PER_PAGE)
-    print("Image col: ", image_col)
-    image_row = calculate_row_pages(image_list, ROW_PER_PAGE)
-    print("Image row: ", image_row)
-    for i in range(0,3):
-        start_x = turtle.xcor()
-        start_y = turtle.ycor()
-        pixel_list = image_list[i]
-        draw_page(turtle, pixel_list)
-        turtle.goto((start_x + (PIXEL_SIZE * COL_PER_PAGE)), start_y)
-    turtle.goto((start_x - COL_PER_PAGE * PIXEL_SIZE * 2), # Why this?
-                (start_y - ROW_PER_PAGE * PIXEL_SIZE))
-    for i in range(3,6):
-        start_x = turtle.xcor()
-        start_y = turtle.ycor()
-        pixel_list = image_list[i]
-        draw_page(turtle, pixel_list)
-        turtle.goto((start_x + (PIXEL_SIZE * COL_PER_PAGE)), start_y)
-    turtle.goto((start_x - 80), start_y - 30)
-    for i in range(6,9):
-        start_x = turtle.xcor()
-        start_y = turtle.ycor()
-        pixel_list = image_list[i]
-        draw_page(turtle, pixel_list)
-        turtle.goto((start_x + (PIXEL_SIZE * COL_PER_PAGE)), start_y)
-    turtle.goto((start_x - 80), start_y - 30)
-
-
-def draw_image_two(turtle, image_list, num_row, num_col):
+def draw_image(turtle, image_list, num_row, num_col):
     ''' Name: draw_image
         Inputs: turtle, image_list (nested list of strings),
                 num_row and num_col -- both ints
         Returns: nothing
     '''
-    j = 0
-    count = 0
-    for i in range(len(image_list)):
-        while j < ((count * num_col) + num_col):
-            start_x = turtle.xcor()
-            start_y = turtle.ycor()
-            pixel_list = image_list[j]
+    start_x = turtle.xcor()
+    start_y = turtle.ycor()
+    row = 1
+    for page in range(len(image_list)):
+        if page < (row * num_col):
+            x_cord = turtle.xcor()
+            y_cord = turtle.ycor()
+            pixel_list = image_list[page]
             draw_page(turtle, pixel_list)
-            turtle.goto((start_x + 40), start_y)
-            j += 1
-        count += 1
-        turtle.goto((start_x - 80), start_y - 30)
+            turtle.goto((x_cord + (COL_PER_PAGE * PIXEL_SIZE), y_cord))
+        else:
+            turtle.goto((start_x), (y_cord - ROW_PER_PAGE * PIXEL_SIZE))
+            x_cord = turtle.xcor()
+            y_cord = turtle.ycor()
+            pixel_list = image_list[page]
+            draw_page(turtle, pixel_list)
+            turtle.goto((x_cord + (COL_PER_PAGE * PIXEL_SIZE), y_cord))
+            row += 1
 
 def main():
+    # Welcome user and ask for image to be rendered
     print("Welcome. Let me render an image for you!")
-##    ask_image_size()
-##    user_selection = collect_image_size(IMAGE_OPTIONS)
-##
-##    compressed_string = compress(user_selection)
-##    decompressed_string = decompress(compressed_string)
-##
+    ask_image_size()
+    user_selection = collect_image_size(IMAGE_OPTIONS)
+
+    # Calculate original image size
+    page_per_row = calculate_row_pages(user_selection, ROW_PER_PAGE)
+    page_per_col = calculate_col_pages(user_selection, COL_PER_PAGE)
+
+    # Compress image for uploading and then decompress
+    compressed_img = compress(user_selection)
+    decompressed_img = decompress(compressed_img)
+
+    # Setup turtle
     screen = turtle.Screen()
-##    screen.tracer(0)
-
-    page_per_row = calculate_row_pages(LITTLE_IMG, ROW_PER_PAGE)
-    page_per_col = calculate_col_pages(LITTLE_IMG, COL_PER_PAGE)
-    
-    compress_img = compress(LITTLE_IMG)
-    decompress_img = decompress(compress_img)
+    screen.tracer(0)
     draw = turtle.Turtle()
-##    draw.hideturtle()
-    draw_image_two(draw, decompress_img, page_per_row, page_per_col)
-##    screen.update()
 
-
+    # Draw image
+    set_start_cord(draw, user_selection, PIXEL_SIZE, ROW_PER_PAGE,
+                   COL_PER_PAGE)
+    draw_image(draw, decompressed_img, page_per_row, page_per_col)
+    screen.update()
 
 main()
-
-# BECAUSE I'M NOT UNPAGINATING THE IMAGE, THESE WON'T WORK
-
-##def determine_x_size(image_list, pixel_size):
-##    ''' Name: determine_x_size
-##        Input: nested list of strings (image_list) and pixel_size (int)
-##        Returns: size of x axis of image in pixels (float)
-##    '''
-##    # len(image_list[0]) is called to determine number of columns/pixels
-##    num_pixels = len(image_list[0])
-##
-##    x_size = num_pixels * pixel_size
-##    return x_size
-##
-##def determine_y_size(original_list, PIXEL_SIZE):
-##    '''
-##        Does: returns verticle size of image (int)
-##    '''
-##    num_pixels = len(original_list)
-##    y_size = num_pixels * PIXEL_SIZE
-##    return y_size
-
-
-# My want to fiddle with this...not need at all?
-def setup_screen(screen_turtle, draw_turtle, original_list, PIXEL_SIZE):
-    screen_turtle.tracer(0)
-##    x_size = determine_x_size(original_list, PIXEL_SIZE)
-##    y_size = determine_y_size(original_list, PIXEL_SIZE)
-##    turtle.setup(x_size, y_size)
-
-def determine_x_start(original_list, PIXEL_SIZE):
-    '''
-        Does: determines starting x cord based on pixel list and size
-    '''
-    x_size = determine_x_size(original_list, PIXEL_SIZE)
-    x_cord = x_size / 2
-    return x_cord
-
-def determine_y_start(original_list, PIXEL_SIZE):
-    '''
-        Does: determines starting y cord based on pixel list and size
-    '''
-    y_size = determine_y_size(original_list, PIXEL_SIZE)
-    y_cord = y_size / 2
-    return y_cord
