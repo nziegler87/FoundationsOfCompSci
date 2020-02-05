139eb55f09d08203768aa332f95f2b185948a7c6
Nathanial Ziegler <ziegler.n@husky.neu.edu>
2020-02-04 21:59:45 -0500

    Finished compress_functions

--- Homework/HW4/compress_functions.py
+++ Homework/HW4/compress_functions.py
@@ -4,104 +4,103 @@
     February 5, 2020
     HW 4
     Description:
-        UPDATE
+        Function to first paginate a nest list of lists of pixels onto pages
+        of four colums and three rows. Then a function to combine streaks
+        of pixels. These are combined into one function to compress an nest
+        list of list of pixels.
 '''
 
+# Pixel constants
 P = "P"
 G = "G"
 Y = "Y"
 L = "L"
 W = "W"
 
-##big = [[P, P, P, P, G, G, G, G],
-##          [P, G, G, G, Y, Y, L, L],
-##          [L, L, L, L, P, P, L, L],
-##          [L, L, P, P, L, L, L, L],
-##          [W, W, W, W, L, L, L, L],
-##          [W, G, G, G, L, L, P, P]]
-##
-##
-##small = [[P, P, P, P],
-##         [P, G, G, G],
-##         [L, L, L, L],
-##         [L, L, P, P],
-##         [W, W, W, W],
-##         [W, G, G, G]]
-
-
+# Constants for the number of rows and columns for each page
 ROW_PER_PAGE = 3
 COL_PER_PAGE = 4
 
-def compress(raw):
+def compress(uncompressed_list):
     ''' Name: compress
-        Input: string of pixels as strings
-        Returns: paginated and compressed list of pixels
+        Input: nested string of strings of pixels
+        Returns: nested string of strings with pixels compressed and paginated
     '''
-    paginated_string = paginate(raw)
-    combined_pixels_string = compile_pixels(paginated_string)
-    return combined_pixels_string
+    # Call the paginate function to group pixels by pages
+    paginated_list = paginate(uncompressed_list)
 
-def paginate(master):
+    # Call the compile_pixels to combine strings of pixels
+    combined_pixels_list = compile_pixels(paginated_list)
+
+    return combined_pixels_list
+
+def paginate(input_string):
     ''' Name: paginate
         Input: list of nested strings
-        Returns: list of of page-specific lists
+        Returns: nested list of strings with pixels grouped by pages
     '''
     final_list = []
 
     # Calculate number of rows and cols in given list
-    num_row = calculate_row(master, ROW_PER_PAGE)
-    num_col = calculate_col(master, COL_PER_PAGE)
+    num_row = calculate_row_pages(input_string, ROW_PER_PAGE)
+    num_col = calculate_col_pages(input_string, COL_PER_PAGE)
 
-    # Iterate through list by sections of 3 rows and 4 colums, appending each section
-    # as new list, which are then appended to master list
+    #Ccreate multipliers to iterate number of row and column pages in list
     for row in range(num_row):
         for col in range(num_col):
             row_mult = row * ROW_PER_PAGE
             col_mult = col * COL_PER_PAGE
-            line = []
+
+            # Create a page of pixels based on 3 row x 4 col page
+            page = []
             for i in range(ROW_PER_PAGE):
                 for j in range(COL_PER_PAGE):
-                    line.append(master[(row_mult + i)][(col_mult + j)])
+                    page.append(input_string[(row_mult + i)][(col_mult + j)])
 
             # Add each each page of pixels to overal list
-            final_list.append(line)
+            final_list.append(page)
 
     return final_list
 
-def calculate_row(original_list, row_per_page):
+def calculate_row_pages(original_list, row_per_page):
     ''' Name: calculate_row
         Input: list of nested strings
-        Returns: number pages based on number of rows (int)
+        Returns: number of row pages based on number of rows (int)
     '''
     num_row = int(len(original_list) / row_per_page)
     return num_row
 
-def calculate_col(original_list, col_per_page):
+def calculate_col_pages(original_list, col_per_page):
     ''' Name: calculate_col
         Input: list of nested strings
-        Returns: number of pages based on number of cols (int)
+        Returns: number of col pages based on number of cols (int)
     '''
-    num_col = int(len(original_list[0]) / col_per_page)
+    pixels = int(len(original_list[0]))
+    num_col = int(pixels / col_per_page)
     return num_col
 
-def compile_pixels(original):
+def compile_pixels(uncompiled):
     ''' Name: compile_pixels
         Input: strings of pixel colors, each item a string
         Returns: updated lists with repeat pixel colors combined
     '''
-    combined_pixels_list = []
+    compiled_pixels_list = []
 
     # Iterate through each list within the list, combining like strings
-    for i in range(len(original)):
-        list_length = len(original[i])
+    for i in range(len(uncompiled)):
+        list_length = len(uncompiled[i])
         count = 1
         combined_pixels = []
-        for j in range(len(original[i])):
-            pixel = str(count) + " " + original[i][j]
+        for j in range(len(uncompiled[i])):
+
+            # Set potential pixel or pixel streak to be added to temp string
+            pixel = str(count) + " " + uncompiled[i][j]
+
+            # Compare each item in list adding streak or pixel to temp string
             if j < (list_length - 1):
                 if list_length == 1:
-                    combined_pixels.append(pixel)
-                elif original[i][j] == original[i][(j + 1)]:
+                    compiled_pixels.append(pixel)
+                elif uncompiled[i][j] == uncompiled[i][(j + 1)]:
                     count += 1
                 else:
                     combined_pixels.append(pixel)
@@ -110,6 +109,6 @@ def compile_pixels(original):
                 combined_pixels.append(pixel)
 
         # Add string of compressed pixels to overal list
-        combined_pixels_list.append(combined_pixels)
+        compiled_pixels_list.append(combined_pixels)
 
-    return combined_pixels_list
+    return compiled_pixels_list
