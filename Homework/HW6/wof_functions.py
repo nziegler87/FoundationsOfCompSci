9cc7ff6f43145e60eb1db11bff878fad59181941
Nathanial Ziegler <ziegler.n@husky.neu.edu>
2020-02-20 20:45:38 -0500

    Update wof_functions.py

--- Homework/HW6/wof_functions.py
+++ Homework/HW6/wof_functions.py
@@ -10,8 +10,6 @@
 '''
 import random
 
-def read_file(
-
 def choose_puzzle(text_file):
     ''' Name: choose_puzzle
         Parameters: name of plain txt file as string - "filename.txt"
@@ -34,3 +32,33 @@ def choose_puzzle(text_file):
     except OSError:
         print("File with gameplay options not found. Please save the wof.txt "
               "file in the same folder as this file and restart program.")
+
+def update_puzzle(character, master_list, user_list):
+    ''' Name: update_puzzle
+        Paramters: a character (str) and two lists of strings, identical length
+        Returns: nothing...modifies user_list in place
+    '''
+    # iterate through each item in master list
+    for i in range(len(master_list)):
+
+        # if character is equal to master_list value, change user_list
+        if character == master_list[i]:
+            user_list[i] = character
+
+def print_puzzle(character_list):
+    ''' Name: print_puzzle
+        Parameters: list of characters
+        Returns: string of characters
+    '''
+    # convert user list to string for return
+    user_string = " ".join(character_string)
+    print(user_string)
+
+def check_match(master_list, user_list):
+    ''' Name: check_match
+        Parameters: two lists of strings, identical in length
+        Returns: True if lists are identical
+    '''
+    if master_list == user_list:
+        return True
+    
