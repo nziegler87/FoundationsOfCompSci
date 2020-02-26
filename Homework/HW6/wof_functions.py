54bb750cf76daa2e02ec8ae39a60cddd8cebbbae
Nathanial Ziegler <ziegler.n@husky.neu.edu>
2020-02-25 19:16:49 -0500

    CLOSER TO FINAL

--- Homework/HW6/wof_functions.py
+++ Homework/HW6/wof_functions.py
@@ -11,14 +11,22 @@
 '''
 import random, string, time, sys
 
+# CONSTANT FOR TXT FILE EXTENSION AND PUZZLE TEXT FILE
 TXT_EXT = ".txt"
+PUZZLE_OPTIONS = "wof.txt"
+
+# CONSTANTS FOR MENU SELECTION
 MENU_LETTERS = ["G", "S"]
+GUESS = "G"
 MENU_OPTIONS = ["Guess a Letter", "Solve"]
+
+# CONSTANTS 
 REGULAR_TURNS = 5
 BONUS_TURNS = 3
-PUZZLE_OPTIONS = "wof.txt"
-BONUS_LETTERS = ["R", "S", "T", "L", "N", "E"]
 BONUS_TIME = 20
+
+BONUS_LETTERS = ["R", "S", "T", "L", "N", "E"]
+
 ZERO_SCORE = 0
 EXCLUDE = list(string.digits + string.punctuation + string.whitespace)
 CENTER_WIDTH = 60
@@ -29,7 +37,10 @@ def print_game_instructions(turns, bonus_status):
                     round, both ints
         Returns: nothing
     '''
+    # join bonus letters list for prnting in bonus round instructions
     bonus_string = join_list(BONUS_LETTERS)
+
+    # print instructions for either regular or bonus play
     if bonus_status is True:
         print("BONUS ROUND".center(CENTER_WIDTH, "-"), "\nIn this round, we "
               "have again selected a random puzzle for you to solve. This "
@@ -52,16 +63,27 @@ def print_game_instructions(turns, bonus_status):
 def join_list(input_list):
     ''' Name: join_list
         Parameters: list of items (int or str) to be joined in series
-        Returns: string with items joined with ", " and ", and" for last item
+        Returns: a string of list items joined together
     '''
+
+    # make blank output string
     output_string = ""
+
+    # iterate through each item in input_list
     for i in range(len(input_list)):
+
+        # if second to last item in list, join with ", and "
         if i == len(input_list) - 2:
             output_string += input_list[i] + ", and "
+
+        # if last item in list, do not add joining text
         elif i == len(input_list) - 1:
             output_string += input_list[i]
         else:
+
+        # if any other item in list, join with ", "
             output_string += input_list[i] + ", "
+
     return output_string
 
 def collect_player_name():
@@ -69,41 +91,43 @@ def collect_player_name():
         Parameters: nothing
         Returns: name of player as a lower-case string
     '''
+
+    # collect player name in lowercase letters
     name = input("Enter your name to log in: ").lower()
+
     return name
 
 def create_filename(extension):
-    ''' Name: convert_to_filename
+    ''' Name: create_filename
         Parameters: filename extension (ie ".txt") as string
         Returns: string with user's name and filename extension concatenated
     '''
     name = collect_player_name()
+    
     return (name + extension)
 
 def return_score(filename):
     ''' Name: return_score
-        Parameters: file name (string) of .txt file, "name.txt", containing number
+        Parameters: filename (string) of .txt file, "name.txt"
+                    containing only a number
         Returns: contents of file or 0 if file doesn't exist, both int
     '''
+    # open and return entire contents of file as one string
     try:
-        # open and return entire contents of file as one string
         with open(filename, "r") as infile:
             contents = infile.read()
+            
+        # return number in file, converted from string to int
+        return int(contents)
 
-        # if file has been corrupt with None, set score to 0
-        if contents == "None":
-            return ZERO_SCORE
-        else:
-            return int(contents)
-
-        # if file doesn't exist, return ZERO_SCORE value
+    # if file doesn't exist, return ZERO_SCORE value
     except OSError:
         return ZERO_SCORE
 
 def get_user_info():
     ''' Name: get_user_info
         Parameters: nothing
-        Returns: tiple with filename of user (string) as first value
+        Returns: tuple with filename of user (string) as first value
                  and their current score (int) as second value
     '''
     filename = create_filename(TXT_EXT)
@@ -115,6 +139,7 @@ def print_score(score):
         Parameters: current score, an int
         Returns: nothing
     '''
+    # if score is 1, puzzle should be singular; else plural
     if score == 1:
         print("\nSo far, you have solved", score, "puzzle.\n")
     else:
@@ -139,6 +164,7 @@ def choose_puzzle():
             puzzle = rand_list[1]
         return (category, puzzle)
 
+    # if file cannot be open, use sys.exit to end program
     except OSError:
         sys.exit("Game options file not found. Please save wof.txt "
                  "in the same folder as this file and the restart program.")
@@ -156,46 +182,50 @@ def make_blank_puzzle(input_string):
         Returns: list where every non-space character is converedted to "_"
     '''
     blank_puzzle = []
-    for i in  range(len(input_string)):
 
-        # if value at position i is anything other than a space, insert "_"
+    # iterate through each value in input string of characters
+    for i in range(len(input_string)):
+
+        # if i is a character other than punctuation or a space, append "_"
         if input_string[i] not in EXCLUDE:
             blank_puzzle.append("_")
 
-        # otherwise, insert a " "
+        # otherwise, insert value of i
         else:
             blank_puzzle.append(input_string[i])
             
     return blank_puzzle
 
-def set_turns(bonus_status):
+def set_turns(bonus):
     ''' Name: set_turns
-        Parameters: bonus, a boolean. True if activated, else false
-        Returns: number of turns, an int
+        Parameters: bonus (boolean) where True if activated, else False
+        Returns: number of turns user has, an int
     '''
-    if bonus_status is True:
+    if bonus is True:
         turns = BONUS_TURNS
     else:
         turns = REGULAR_TURNS
+
     return turns
 
-def pre_solve(bonus_status, user_puzzle, master_string):
+def pre_solve(bonus, user_puzzle, master_string):
     ''' Name: pre_solve
-        Parameters: list of characters to presolve puzzle - a string & list
-                    of strings of the same length and bonus status, boolean
-        Returns: Nothing...modifies list in place
+        Parameters: bonus state (a boolean), puzzle that user is trying to
+                    solve (string of characters), and master_string or
+                    the computer-generated puzzle user is trying to guess
+        Returns: Nothing; modifies list in place
     '''
-    if bonus_status is True:
+    if bonus is True:
         # iterate through each item in master string
         for i in range(len(master_string)):
 
-            # if character is in letters_list, update user_list
+            # if character is in BONUS_LETTERS, update user_list
             if master_string[i] in BONUS_LETTERS:
                 user_puzzle[i] = master_string[i]
 
 def print_puzzle_info(category, puzzle_list):
     ''' Name: print_puzzle_info
-        Parameters: puzzle category (string) list of puzzle characters
+        Parameters: puzzle category, string, and list of puzzle characters
         Returns: nothing
     '''
     print("Your puzzle category is:", category)
@@ -205,7 +235,7 @@ def display_menu(menu_letters, menu_options):
     ''' Name: display_menu
         Parameters: two lists of strings, identical in length -- one with
                     single letters and other with descriptor for each menu item
-        Returns: user choice, a string, validated against menu_select
+        Returns: user choice, a string, validated against menu_letters
     '''
     while True:
         
@@ -213,24 +243,32 @@ def display_menu(menu_letters, menu_options):
         for i in range(len(menu_letters)):
             print("     ", menu_letters[i], " -- ", menu_options[i], sep = "")
 
-        # collect choice and validate against menu letters
+        # collect choice and validate against menu_letters
         choice = input("Enter your selection: ").upper()
         if choice not in menu_letters:
             print("\nInvalid selection. Try again.")
             continue
+
         else:
             return choice
 
 def let_player_guess(turns, puzzle, display_puzzle):
-    ''' Name: let_play_guess
+    ''' Name: let_player_guess
         Parameters: number of turns (int), correct puzzle (string),
                     list of characters for display_puzzle
         Returns: nothing
     '''
+    # allow user to guess while they still have turns available
     while turns > 0:
+
+        # collect choice from user using display_menu function
         choice = display_menu(MENU_LETTERS, MENU_OPTIONS)
-        if choice == "G":
+
+        # if user selects guess, update puzzle they are solving and reduce turns
+        if choice == GUESS:
             turns = process_guess(puzzle, display_puzzle, turns)
+
+        # if they run out of turns or select solve, break loop
         else:
             break
 
@@ -240,18 +278,21 @@ def collect_guess():
         Returns: a single character, a string
     '''
     guess = input("Enter your guess: ").upper()
-    if len(guess) > 1:
-        guess = input("You can only guess one character at a time. "
-                      "Try again: ").upper()
-    else:
-        return guess
+
+    # validate so they only enter one character
+    while True:
+        if len(guess) > 1:
+            guess = input("You can only guess one character at a time. "
+                          "Try again: ").upper()
+        else:
+            return guess
 
 def update_puzzle(character, master_string, user_list):
     ''' Name: update_puzzle
         Paramters: a character (str), a string & list of strings of same length
         Returns: nothing...modifies user_list in place
     '''
-    # iterate through each item in master string
+    # iterate through each item in master string, or computer-generated puzzle
     for i in range(len(master_string)):
 
         # if character is equal to master_string value, change user_list
@@ -263,10 +304,13 @@ def print_remaining_turns(count):
         Parameters: number of turns remaining, an int
         Returns: nothing
     '''
+    # check if turns should be printed as singular or plural
     if count > 1:
         print("You have", count, "turns remaining.")
     elif count > 0:
         print("You have", count, "turn remaining.")
+
+    # let user know they are out of turns
     else:
         print("You are out of turns.")
 
@@ -275,21 +319,28 @@ def print_puzzle(character_list):
         Parameters: list of characters
         Returns: nothing
     '''
-    # convert user list to string for return
+    # convert user list to string and then print
     user_string = " ".join(character_list)
     print("\n", user_string, "\n", sep = "")
 
-def process_guess(puzzle, display_puzzle, turns):
+def process_guess(master_string, display_puzzle, turns):
     ''' Name: process_guess
-        Parameters: current puzzle (str), display_puzzle (list of strings),
-                    and turns (int)
+        Parameters: master_string or computer selected puzzle (str),
+                    display_puzzle (list of strings), and turns (int)
         Returns: number of turns remaining (int)
     '''
+    # use collect_guess function to collect guess
     guess = collect_guess()
-    update_puzzle(guess, puzzle, display_puzzle)
+
+    # update list of strings user is trying to fill in with guess and print
+    update_puzzle(guess, master_string, display_puzzle)
     print_puzzle(display_puzzle)
+
+    # decrease turns and print number of turns left
     turns -= 1
     print_remaining_turns(turns)
+
+    # return value of turns
     return turns
 
 def check_match(master_string, user_string):
@@ -310,59 +361,77 @@ def calculate_time_elapsed(start_time, end_time):
 def collect_final_guess(bonus):
     ''' Name: collect_final_guess
         Parameters: a boolean indicating if bonus round is active
-        Returns: user guess and time it took to respond as tupil
+        Returns: user guess (str) and time (int) for user to respond as tuple
     '''
+    # if bonus is active, let user know how many seconds they have to solve
     if bonus is True:
         print("You have", BONUS_TIME, "seconds to enter "
                   "your guess, starting now.")
+
+    # capture start time
     start_time = time.time()
+
+    # collect user guess in uppercase
     final_guess = input("Enter your final guess: ").upper()
+
+    # capture end time
     end_time = time.time()
+
+    # calculate total time it took user to respond
     total_time = calculate_time_elapsed(start_time, end_time)
+    
     return (final_guess, total_time)
 
-def print_regular_results(user_guess, computer_string, score):
+def print_regular_results(user_guess, master_string, score):
     ''' Name: print_regular_results
-        Parameters: user_guess and computer_string (strs), running score (int)
+        Parameters: user_guess and master_string (strs), running score (int)
         Returns: nothing
     '''
-    if user_guess == computer_string and score == 1:
+    # print congrats message with correct plural/nonplural of puzzles
+    if check_match(user_guess, master_string) and score == 1:
             print("\nCongrats! You you have now solved", score, "puzzle!\n")
-    elif user_guess == computer_string and score != 1:
+    elif check_match(user_guess, master_string) and score != 1:
         print("\nCongrats! You have now solved", score, "puzzles!\n")
+
+    # if guess does not match, display user the string
     else:
         print("\nI'm sorry, that wasn't correct. Your puzzle was:\n",
-              computer_string, "\n", sep = "")
+              master_string, "\n", sep = "")
 
-def print_bonus_results(bonus, total_time, user_guess, computer_string, score):
+def print_bonus_results(bonus, total_time, user_guess, master_string, score):
     ''' Name: print_bonus_results
         Parameters: bonus status, a boolean, response time (float),
-                    user_guess and computer_string (strs), running score (int)
+                    user_guess and master_string (strs), running score (int)
         Returns: nothing
     '''
-    if check_match(user_guess, computer_string) and total_time <= BONUS_TIME:
-        if user_guess == computer_string and score == 1:
-            print("\nCongrats! You you have now solved", score, "puzzle!\n")
-        elif user_guess == computer_string and score != 1:
-            print("\nCongrats! You have now solved", score, "puzzles!\n")
-    elif check_match(user_guess, computer_string) and total_time > BONUS_TIME:
+    # if guess is correct and less than time alloted display congrats
+    if check_match(user_guess, master_string) and total_time <= BONUS_TIME:
+        print_regular_results(user_guess, master_string, score)
+
+    # if match was correct but they used too much time, display message
+    elif check_match(user_guess, master_string) and total_time > BONUS_TIME:
         print("I'm sorry. That was correct but you ran out of time. "
               "It took you you", round(total_time, 2), "seconds to "
               "respond.\n")
+
+    # if guess was incorrect, display correct string
     else:
         print("I'm sorry that was not correct. Your puzzle was:\n",
-                  computer_string, "\n", sep = "")
+                  master_string, "\n", sep = "")
 
-def print_game_results(bonus, total_time, user_guess, computer_string, score):
+def print_game_results(bonus, total_time, user_guess, master_string, score):
     ''' Name: print_game_results
         Parameters: bonus status, a boolean, response time (float),
-                    user_guess and computer_string (strs), running score (int)
+                    user_guess and master_string (strs), running score (int)
         Returns: nothing
     '''
+    # if bonus is not activated, call print_regular_results
     if bonus is False:
-        print_regular_results(user_guess, computer_string, score)
+        print_regular_results(user_guess, master_string, score)
+
+    # if bonus is activated, call print_bonus_results
     else:
-        print_bonus_results(bonus, total_time, user_guess, computer_string,
+        print_bonus_results(bonus, total_time, user_guess, master_string,
                             score)
 
 def flip_boolean(boolean_a):
@@ -379,9 +448,12 @@ def update_score(bonus, score, user_string, master_string, total_time):
                 and total time it took user to respond (float)
         Returns: updated score (int)
     '''
+    # if bonus is not activated, simply compare scores and update score
     if bonus is False:
         if check_match(user_string, master_string):
             score += 1
+
+    # if in bonus round, factor in time it took for user to respond
     else:
         if check_match(user_string, master_string) and total_time <= BONUS_TIME:
             score += 1
@@ -390,9 +462,18 @@ def update_score(bonus, score, user_string, master_string, total_time):
 
 def update_continue_status(bonus, another_round, user_string, master_string,
                            total_time):
+    ''' Name: update_continue_status
+        Parameters: bonus and another_round statuses (booleans), user_string
+                    (a string of their guess), master_string (string of
+                    random puzzle) and total time to respond (float)
+        Returns: status of bonus and another_round booleans as a tuple
+    '''
+    # if not a bonus round and guess is correct, allow next turn as bonus round
     if bonus is False and user_string == master_string:
         bonus = True
         another_round = True
+
+    # If guess is wrong or already completed bonus round, do not allow continue
     else:
         bonus = False
         another_round = False
--- Homework/HW6/wof_with_bonus_driver.py
+++ Homework/HW6/wof_with_bonus_driver.py
@@ -8,39 +8,31 @@
 '''
 
 import time
-from wof_functions import (choose_puzzle, make_blank_puzzle, print_puzzle,
-                           print_game_results, collect_guess, update_puzzle,
-                           create_filename, display_menu, return_score,
-                           print_score, print_remaining_turns, write_score,
-                           check_match, process_guess, pre_solve, set_turns,
-                           print_game_instructions, join_list, get_user_info,
-                           collect_player_name, print_puzzle_info,
-                           let_player_guess, collect_final_guess,
-                           print_game_results, update_score, flip_boolean,
-                           update_continue_status)
+from wof_functions import *
 
 def main():
-    # collect filename, display current score, start with no bonus round
+    # collect user info, display their stats, and start with no bonus round
     filename, score = get_user_info()
     print_score(score)
     bonus = False
     another_round = True
     
     while another_round is True:
-        # set number of turns and display instructions based on bonus status
+        # based on bonus status, set number of turns and display instructions
         turns = set_turns(bonus)
         print_game_instructions(turns, bonus)
 
-        # pick random puzle, create blank puzzle, display to user
+        # pick random puzzle, create blank puzzle for guessing,
+        # presolve if in bonus round, and display to user
         category, puzzle = choose_puzzle()
         display_puzzle = make_blank_puzzle(puzzle)
         pre_solve(bonus, display_puzzle, puzzle)
         print_puzzle_info(category, display_puzzle)
 
-        # game play where user guesses or solves
+        # game play where user selects guess or solve or runs out of turns
         let_player_guess(turns, puzzle, display_puzzle)
 
-        # process final guess - update score, game play, and print results
+        # get final guess, update score, print results, update next round/bonus 
         final_guess, total_time = collect_final_guess(bonus)
         score = update_score(bonus, score, final_guess, puzzle, total_time)
         print_game_results(bonus, total_time, final_guess, puzzle, score)
