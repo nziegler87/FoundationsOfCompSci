90de9910b4465c6507c9eaee2fd25e73a08c78ae
Nathanial Ziegler <ziegler.n@husky.neu.edu>
2020-03-19 19:54:48 -0400

    Update cookie_clicker.py

---
+++ Homework/HW7/cookie_clicker.py
@@ -0,0 +1,18 @@
+'''
+    CS 5001
+    Nathanial Ziegler
+    March 2020
+    HW 7
+    Description:
+        NEED TO UPDATE
+'''
+
+import turtle
+
+COOKIE = "cc_cookie.gif"
+
+
+def main():
+    
+
+main()
