92d5d2028c3ab30a9d545a79efec3a113c7097c9
Nathanial Ziegler <ziegler.n@husky.neu.edu>
2020-03-20 16:58:15 -0400

    Update sentence.py

--- Reading/Chapter 10/sentence.py
+++ Reading/Chapter 10/sentence.py
@@ -32,3 +32,25 @@ class Sentence2:
 
     def get_num_words(self):
         return len(self.__string_list)
+
+class Sentence3:
+    def __init__(self, string):
+        self.string = string
+
+    def capitalize(self):
+        self.string = self.string.upper()
+
+    def lowercase(self):
+        self.string = self.string.lower()
+
+    def add_punctuation(self, punctuation):
+        self.string = self.string + punctuation
+
+    def __str__(self):
+        return self.string
+##        printable = "What a beautiful day it is! \nYour string is " + \
+##                    self.string
+##        return printable
+
+    def __len__(self):
+        return len("Why do I try?")
